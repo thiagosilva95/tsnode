386387cf8b6d695b9b0466573fd4441547995d4b
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-06-04 00:05:36 -0300

    Updating

--- startUp.ts
+++ startUp.ts
@@ -24,9 +24,12 @@ class StartUp {
             methods: "GET, OPTIONS, PUT, POST, DELETE",
             origin: "*"
         }
+
+        this.app.use(cors(options));
     }
 
     middler() {
+        this.enableCors();
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
     }
