8c2b2631d2186666cd091af07b9bfbbc075bb7c9
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-06-07 18:16:47 -0300

    Upload files

---
+++ infra/uploads.ts
@@ -0,0 +1,10 @@
+import * as multer from 'multer';
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, '/uploads');
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
