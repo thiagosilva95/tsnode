b926cecc24a4219f23ca901f09369b20758c1106
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-06-15 13:07:26 -0300

    Adding compression

--- startUp.ts
+++ startUp.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
+import * as compression from 'compression';
 
 import Database from './infra/db';
 import Auth from './infra/auth';
@@ -34,6 +35,7 @@ class StartUp {
         this.enableCors();
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(compression());
     }
 
     routes() {
